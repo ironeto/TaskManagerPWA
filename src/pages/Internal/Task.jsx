99cc4ccf38e25e5d7c52f0bfd5f5cf9908f7eee5
alvaroaugustodemarconeto <alvaro.marco.neto@gmail.com>
2023-10-05 11:19:30 +0200

    validations

--- src/pages/Internal/Task.jsx
+++ src/pages/Internal/Task.jsx
@@ -12,6 +12,7 @@ import {
   FormControlLabel,
   FormGroup,
   Switch,
+  Typography,
 } from '@material-ui/core';
 
 const Task = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
@@ -24,6 +25,14 @@ const Task = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
   const [concluded, setConcluded] = useState(false);
+
+  const [titleError, setTitleError] = useState('');
+  const [dayError, setDayError] = useState('');
+  const [hourError, setHourError] = useState('');
+  const [hourEndError, setHourEndError] = useState('');
+  const [categoryError, setCategoryError] = useState('');
+
+  const [formIsValid, setFormIsValid] = useState(true); // Track form validity
 
   const _loadTask = async () => {
     const task = await loadTask(firebaseApp, params.id);
@@ -44,29 +53,89 @@ const Task = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
     }
   }, []);
 
-  const saveOrUpdate = async () => {
-    const data = {
-      title,
-      day,
-      hour,
-      hourEnd,
-      category,
-      description,
-      concluded,
-    };
-    if (params.id) {
-      data.id = params.id;
+  // Validation function to check if all required fields are filled
+  const validateForm = () => {
+    let isValid = true;
+
+    if (!title) {
+      setTitleError('Título é obrigatório');
+      isValid = false;
+    } else {
+      setTitleError('');
     }
-    await saveTask(firebaseApp, data);
-    navigate('/');
+
+    if (!day) {
+      setDayError('Data é obrigatória');
+      isValid = false;
+    } else {
+      setDayError('');
+    }
+
+    if (!hour) {
+      setHourError('Hora Início é obrigatória');
+      isValid = false;
+    } else {
+      setHourError('');
+    }
+
+    if (!hourEnd) {
+      setHourEndError('Hora Fim é obrigatória');
+      isValid = false;
+    } else {
+      setHourEndError('');
+    }
+
+    if (!category) {
+      setCategoryError('Categoria é obrigatória');
+      isValid = false;
+    } else {
+      setCategoryError('');
+    }
+
+    return isValid;
+  };
+
+  const saveOrUpdate = async () => {
+    const isFormValid = validateForm();
+    if (isFormValid) {
+      const data = {
+        title,
+        day,
+        hour,
+        hourEnd,
+        category,
+        description,
+        concluded,
+      };
+      if (params.id) {
+        data.id = params.id;
+      }
+      await saveTask(firebaseApp, data);
+      navigate('/');
+    } else {
+      // Handle the case where the form is not valid
+      setFormIsValid(false);
+    }
   };
 
   return (
     <>
-      <TopComponent hasMenu={false} hasArrowBack={true} hasImage={false} title={`Nova Task`} subtitle={'Crie sua tarefa...'} />
+      <TopComponent
+        hasMenu={false}
+        hasArrowBack={true}
+        hasImage={false}
+        title={`Nova Task`}
+        subtitle={'Crie sua tarefa...'}
+      />
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(100% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(100% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
@@ -86,59 +155,162 @@ const Task = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
       </BoxComponent>
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(100% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(100% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
-        <TextFieldComponent variant="standard" fullWidth={true} label="Titulo" value={title} type="text" onChange={(e) => setTitle(e.target.value)}/>
+        <TextFieldComponent
+          variant="standard"
+          fullWidth={true}
+          label="Titulo"
+          InputLabelProps={{ shrink: true }}
+          value={title}
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        {!formIsValid && titleError && (
+          <Typography variant="caption" color="error">
+            {titleError}
+          </Typography>
+        )}
       </BoxComponent>
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(100% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(100% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
-        <TextFieldComponent variant="standard" fullWidth={true} label="" value={day} type="date" onChange={(e) => setDay(e.target.value)}/>
+        <TextFieldComponent
+          variant="standard"
+          fullWidth={true}
+          label="Data"
+          InputLabelProps={{ shrink: true }}
+          value={day}
+          type="date"
+          onChange={(e) => setDay(e.target.value)}
+        />
+        {!formIsValid && dayError && (
+          <Typography variant="caption" color="error">
+            {dayError}
+          </Typography>
+        )}
       </BoxComponent>
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(50% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(50% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
-        <TextFieldComponent variant="standard" fullWidth={true} label="Hora Inicio" value={hour} type="time" onChange={(e) => setHour(e.target.value)}/>
+        <TextFieldComponent
+          variant="standard"
+          fullWidth={true}
+          label="Hora Inicio"
+          value={hour}
+          InputLabelProps={{ shrink: true }}
+          type="time"
+          onChange={(e) => setHour(e.target.value)}
+        />
+        {!formIsValid && hourError && (
+          <Typography variant="caption" color="error">
+            {hourError}
+          </Typography>
+        )}
       </BoxComponent>
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(50% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(50% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
-        <TextFieldComponent variant="standard" fullWidth={true} label="Hora Fim" value={hourEnd} type="time" onChange={(e) => setHourEnd(e.target.value)}/>
+        <TextFieldComponent
+          variant="standard"
+          fullWidth={true}
+          label="Hora Fim"
+          InputLabelProps={{ shrink: true }}
+          value={hourEnd}
+          type="time"
+          onChange={(e) => setHourEnd(e.target.value)}
+        />
+        {!formIsValid && hourEndError && (
+          <Typography variant="caption" color="error">
+            {hourEndError}
+          </Typography>
+        )}
       </BoxComponent>
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(100% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(100% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
-        <TextFieldComponent variant="standard" fullWidth={true} label="Categoria" value={category} type="text" onChange={(e) => setCategory(e.target.value)}/>
+        <TextFieldComponent
+          variant="standard"
+          fullWidth={true}
+          label="Categoria"
+          InputLabelProps={{ shrink: true }}
+          value={category}
+          type="text"
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        {!formIsValid && categoryError && (
+          <Typography variant="caption" color="error">
+            {categoryError}
+          </Typography>
+        )}
       </BoxComponent>
+      {/* ... (other form components) */}
       <BoxComponent
         component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(100% - 64px)', float: 'left' }}
+        sx={{
+          pl: 4,
+          pr: 4,
+          marginTop: 4,
+          width: 'calc(100% - 64px)',
+          float: 'left',
+        }}
         noValidate={true}
         autoComplete={'off'}
       >
-        <TextFieldComponent rows={4} multiline variant="standard" fullWidth={true} label="Descricao" value={description} type="text" onChange={(e) => setDescription(e.target.value)}/>
-      </BoxComponent>
-      <BoxComponent
-        component="div"
-        sx={{ pl: 4, pr: 4, marginTop: 4, width: 'calc(100% - 64px)', float: 'left' }}
-        noValidate={true}
-        autoComplete={'off'}
-      >
-        <ButtonComponent fullWidth={true} label={params.id ? 'ATUALIZAR' : 'SALVAR'} onClick={saveOrUpdate} />
+        <ButtonComponent
+          fullWidth={true}
+          label={params.id ? 'ATUALIZAR' : 'SALVAR'}
+          onClick={saveOrUpdate}
+        />
+        {!formIsValid && (
+          <Typography variant="body2" color="error">
+            Please fill in all required fields.
+          </Typography>
+        )}
       </BoxComponent>
     </>
   );
