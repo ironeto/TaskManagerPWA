a2ab51a8e7284b5429791de3041858eb86a7859b
alvaroaugustodemarconeto <alvaro.marco.neto@gmail.com>
2023-10-05 13:19:12 +0200

    calendar

--- src/pages/Internal/Timeline.jsx
+++ src/pages/Internal/Timeline.jsx
@@ -5,6 +5,12 @@ import { TopComponent } from "../../components";
 import Scheduler from "react-mui-scheduler";
 import { getTasks } from '../../utils/task';
 import { DataModel } from "../../data/datamodel";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment)
 
 const Timeline = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
     const navigate = useNavigate();
@@ -13,18 +19,13 @@ const Timeline = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
 
     const listTasks = async () => {
         const response = await getTasks(firebaseApp);
-        const user = await dataModelUser.getLocal({ synced: true });
         const tasksCalendar = response.map((task, index) => {
             return {
                 id: task.id,
-                label: task.title,
-                groupLabel: task.category,
-                user: user[0].email,
-                color: "#f28f6a",
-                startHour: task.hour,
-                endHour: task.hourEnd,
-                date: task.day,
-                createdBy: user[0].email
+                title: task.title,
+                start: new Date(`${task.day}T${task.hour}`),
+                end:  new Date(`${task.day}T${task.hourEnd}`),
+                desc: task.description
               }
         });
 
@@ -36,55 +37,25 @@ const Timeline = ({ setCurrentPath, loggoutRoutes, firebaseApp }) => {
         verifyLogin(loggoutRoutes, window.location.pathname, navigate, firebaseApp);
         listTasks();
     }, []);
-
-
-    const [state] = useState({
-        options: {
-          transitionMode: "zoom", // or fade
-          startWeekOn: "Mon", // or Sun
-          defaultMode: "month", // or week | day | timeline
-          minWidth: 540,
-          maxWidth: 540,
-          minHeight: 540,
-          maxHeight: 540
-        },
-        alertProps: {
-          open: false,
-          color: "info", // info | success | warning | error
-          severity: "info", // info | success | warning | error
-          message: "Task Calendar",
-          showActionButton: false,
-          showNotification: false,
-          delay: 1500
-        },
-        toolbarProps: {
-          showSearchBar: true,
-          showSwitchModeButtons: false,
-          showDatePicker: false
-        }
-      });
-           
-      const handleEventClick = (event, item) => {
-        navigate(`/task/${item.id}`);
-      };
-        
-    useEffect(() => {
-        setCurrentPath(window.location.pathname)
-        verifyLogin(loggoutRoutes, window.location.pathname, navigate, firebaseApp)
-    }, []);
+     
+      const handleEventClick = (event) => {
+        navigate(`/task/${event.id}`);
+      };      
      
     return <>
         <TopComponent hasMenu={true} hasArrowBack={true} hasImage={false} title={`Calendário`}/>
-        <div style={{ marginTop: '20px' }}> {/* Add margin top here */}
-      <Scheduler
-        locale="en"
-        events={tasks}
-        legacyStyle={false}
-        options={state?.options}
-        alertProps={state?.alertProps}
-        toolbarProps={state?.toolbarProps}
-        onTaskClick={handleEventClick}
-      />
+        <div style={{ marginTop: '20px' }}>
+        <div>
+    <Calendar
+      localizer={localizer}
+      defaultView="month"
+      events={tasks}
+      startAccessor="start"
+      endAccessor="end"
+      onSelectEvent={handleEventClick}
+      style={{ height: 600 }}
+    />
+  </div>
     </div>
     </>
 }
